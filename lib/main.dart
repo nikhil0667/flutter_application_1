c032f09a74f374eee13f00f60345aad9d73dca33
Nikhil <nikhilkhambhayta@gmail.com>
2023-02-18 23:29:13 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,19 +13,19 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Radhe infotech"),
-      ),
-      // ignore: avoid_unnecessary_containers
-      body: Center(
-        child: Container(
-          alignment: Alignment.center,
-          color: const Color.fromARGB(255, 16, 129, 182),
-          height: 200,
-          width: 200,
-          child: Text("I love you"),
+        appBar: AppBar(
+          title: const Text("Radhe infotech"),
         ),
-      ),
-    );
+        // ignore: avoid_unnecessary_containers
+        body: Center(
+          child: Container(
+            alignment: Alignment.center,
+            color: const Color.fromARGB(255, 16, 129, 182),
+            height: 200,
+            width: 200,
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
+            child: const Text("I love you"),
+          ),
+        ));
   }
 }
