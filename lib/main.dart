cbc07e1e7476a49453ef6e114fa79620c5a75068
Nikhil <nikhilkhambhayta@gmail.com>
2023-02-19 20:41:06 +0530

    Ch-2 Complete

--- lib/main.dart
+++ lib/main.dart
@@ -17,36 +17,36 @@ class Homepage extends StatelessWidget {
         title: const Text("Radhe infotech"),
       ),
       // ignore: avoid_unnecessary_containers
-      body: Container(
-        color: Colors.blue,
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        child: SizedBox(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Container(
-
-                width: 100,
-                height: 100,
-                color: Colors.orange,
-              ),
-              Container(
-
-                // margin: const EdgeInsets.only(left: 10.0,right: 10.0),
-                width: 100,
-                height: 100,
-                color: Colors.white,
-              ),
-              Container(
-
-                // margin: const EdgeInsets.only(left: 10.0,right: 10.0),
-                width: 100,
-                height: 100,
-                color: Colors.green,
-              ),
-            ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          color: Colors.orange.shade300,
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: SizedBox(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.red,
+                ),
+                Container(
+                  // margin: const EdgeInsets.only(left: 10.0,right: 10.0),
+                  width: 100,
+                  height: 100,
+                  color: Colors.white,
+                ),
+                Container(
+                  // margin: const EdgeInsets.only(left: 10.0,right: 10.0),
+                  width: 100,
+                  height: 100,
+                  color: Colors.green,
+                ),
+              ],
+            ),
           ),
         ),
       ),
